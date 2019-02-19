bc730c62b2a76756e802d169ec0d2724cd526ffb
Vadász Péter <pevad95@gmail.com>
2019-02-19 22:25:31 +0100

    Smaller fixes.

--- hexa_octa.cpp
+++ hexa_octa.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <bitset>
 
 int main() {
 	int hexa = 0xff;
 	int hexa2 = 0x1;
 	std::cout << hexa << std::endl;
 	std::cout << hexa + hexa2 << std::endl;
+	std::cout << std::bitset<8>(hexa) << std::endl;
 	
 	int octa = 010;
 	std::cout << octa << std::endl;
